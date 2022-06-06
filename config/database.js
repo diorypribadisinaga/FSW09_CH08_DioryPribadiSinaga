00efe7c755279b7dfac4eb5700bfda47857c099b
dioripribadisinaga@gmail.com <dioripribadisinaga@gmail.com>
2022-06-06 23:12:33 +0700

    op

--- config/database.js
+++ config/database.js
@@ -37,8 +37,8 @@ module.exports = {
   },
   production: {
     username: 'dnlsiokuvoktbz',
-    password: 'DB_PASSWORD=0eaf618ed3b09f76a0cca8d3ca87c87c6b6744e9e5ded75ba674baf8c399404c',
-    database: 'postgresql-vertical-01581',
+    password: '0eaf618ed3b09f76a0cca8d3ca87c87c6b6744e9e5ded75ba674baf8c399404c',
+    database: 'd96qq9h02ocj2c',
     host: 'ec2-52-44-13-158.compute-1.amazonaws.com',
     port: '5432',
     dialect: "postgres",
